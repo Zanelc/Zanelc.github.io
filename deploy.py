b9b4490e4b5c89e35c6440c55e1f372b2ce48f7c
Zanelc <1666839736@qq.com>
2023-07-27 00:10:34 +0800

    source update

--- deploy.py
+++ deploy.py
@@ -5,10 +5,10 @@ source_cmd='git add . && git commit -m "source update" && git push origin source
 try:
     print("---------网页文件PUSH---------")
     os.system(static_cmd)
-    print("网页push成功！")
+    print("---------网页PUSH成功---------")
     print("\n\n")
     print("---------代码文件PUSH---------")
     os.system(source_cmd)
-    print("源代码push成功！")
+    print("---------源代码PUSH成功---------")
 except:
     print("部署失败，请手动检查情况！")
