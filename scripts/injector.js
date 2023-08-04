0273dba410a04abfcbee858febe49490de70196d
Zanelc <1666839736@qq.com>
2023-08-05 02:29:27 +0800

    source update

--- scripts/injector.js
+++ scripts/injector.js
@@ -2,3 +2,5 @@
 hexo.extend.injector.register('body_end', '<script src="/js/time.js"></script>', 'default');
 // emoji点击效果
 hexo.extend.injector.register('body_end', '<script src="/js/emoji.js"></script>', 'default');
+// 网站运行时长
+hexo.extend.injector.register('body_end', '<script src="/js/duration.js"></script>', 'default');
